59a3e34c55e3346cc239a3b29fab5414ea31c08f
Brandon Risberg <brandon.risberg@gmail.com>
2017-06-11 23:13:55 -0700

    Basic Sample Character structure

---
+++ mod/scripts/prefabs/wdn_wendolyn.lua
@@ -0,0 +1,58 @@
+
+local MakePlayerCharacter = require "prefabs/player_common"
+
+
+local assets = {
+
+        Asset( "ANIM", "anim/player_basic.zip" ),
+        Asset( "ANIM", "anim/player_idles_shiver.zip" ),
+        Asset( "ANIM", "anim/player_actions.zip" ),
+        Asset( "ANIM", "anim/player_actions_axe.zip" ),
+        Asset( "ANIM", "anim/player_actions_pickaxe.zip" ),
+        Asset( "ANIM", "anim/player_actions_shovel.zip" ),
+        Asset( "ANIM", "anim/player_actions_blowdart.zip" ),
+        Asset( "ANIM", "anim/player_actions_eat.zip" ),
+        Asset( "ANIM", "anim/player_actions_item.zip" ),
+        Asset( "ANIM", "anim/player_actions_uniqueitem.zip" ),
+        Asset( "ANIM", "anim/player_actions_bugnet.zip" ),
+        Asset( "ANIM", "anim/player_actions_fishing.zip" ),
+        Asset( "ANIM", "anim/player_actions_boomerang.zip" ),
+        Asset( "ANIM", "anim/player_bush_hat.zip" ),
+        Asset( "ANIM", "anim/player_attacks.zip" ),
+        Asset( "ANIM", "anim/player_idles.zip" ),
+        Asset( "ANIM", "anim/player_rebirth.zip" ),
+        Asset( "ANIM", "anim/player_jump.zip" ),
+        Asset( "ANIM", "anim/player_amulet_resurrect.zip" ),
+        Asset( "ANIM", "anim/player_teleport.zip" ),
+        Asset( "ANIM", "anim/wilson_fx.zip" ),
+        Asset( "ANIM", "anim/player_one_man_band.zip" ),
+        Asset( "ANIM", "anim/shadow_hands.zip" ),
+        Asset( "SOUND", "sound/sfx.fsb" ),
+        Asset( "SOUND", "sound/wilson.fsb" ),
+        Asset( "ANIM", "anim/beard.zip" ),
+
+        Asset( "ANIM", "anim/wtw_whitney.zip" ),
+}
+local prefabs = {}
+local start_inv = {
+	-- Custom starting items
+}
+
+local fn = function(inst)
+
+	-- choose which sounds this character will play
+	inst.soundsname = "willow"
+
+	-- Minimap icon
+	inst.MiniMapEntity:SetIcon( "wdn_wendolyn.tex" )
+
+	-- Stats
+	inst.components.health:SetMaxHealth(150)
+	inst.components.hunger:SetMax(150)
+	inst.components.sanity:SetMax(200)
+
+	-- Damage multiplier (optional)
+  inst.components.combat.damagemultiplier = 1
+end
+
+return MakePlayerCharacter("wdn_wendolyn", prefabs, assets, fn, start_inv)
