40da9e79dede740b7341c39d76220784d0d2aa16
Brandon Risberg <brandon.risberg@gmail.com>
2017-06-13 22:31:05 -0700

    added basic sound setup and wendolyn voice

--- mod/modmain.lua
+++ mod/modmain.lua
@@ -17,6 +17,9 @@ Assets = {
 
 	Asset( "IMAGE", "images/map_icons/wdn_wendolyn.tex" ),
 	Asset( "ATLAS", "images/map_icons/wdn_wendolyn.xml" ),
+
+	Asset("SOUNDPACKAGE", "sound/wdn_wendolyn_speech.fev"),
+	Asset("SOUND", "sound/wdn_wendolyn_speech.fsb"),
 }
 
 local require = GLOBAL.require
@@ -33,6 +36,8 @@ GLOBAL.STRINGS.CHARACTERS.WDN_WENDOLYN = require "speech_wdn_wendolyn"
 -- Let the game know character is male, female, or robot
 table.insert(GLOBAL.CHARACTER_GENDERS.FEMALE, "wdn_wendolyn")
 
-
+RemapSoundEvent( "dontstarve/characters/wdn_wendolyn/death_voice", "wdn_wendolyn_speech/characters/wdn_wendolyn/death_voice" )
+RemapSoundEvent( "dontstarve/characters/wdn_wendolyn/hurt", "wdn_wendolyn_speech/characters/wdn_wendolyn/hurt" )
+RemapSoundEvent( "dontstarve/characters/wdn_wendolyn/talk_LP", "wdn_wendolyn_speech/characters/wdn_wendolyn/talk_LP" )
 AddMinimapAtlas("images/map_icons/wdn_wendolyn.xml")
 AddModCharacter("wdn_wendolyn")
